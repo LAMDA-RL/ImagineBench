945f23cfb37241188a2c352bcbe6cabfd373ac7b
pangjc <pangjc@lamda.nju.edu.cn>
2025-05-12 16:46:51 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='rimro',
+    name='imagine_bench',
     version='0.0.1',
     packages=setuptools.find_packages(),
     install_requires=[
